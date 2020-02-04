3daab74c6cbd5131248fb95b15566b4c0b89d6f0
CryJ <crystal.jimenez11@gmail.com>
2020-02-04 15:10:52 -0800

    got var questions

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 'use strict'
 
 var fname = prompt('What is your first name?');
+var lname = prompt('What is you last name');
+var dog = prompt('Did I have a chihuahua?');
+var school = prompt('Have i been in private school until middle school?');
+var job = prompt('Was my third job L.A. Fitness?');
+var career = prompt('Is my main goal to start a career?');
+var hobby = prompt('Do I want to try and create pastries as a hobby?');
